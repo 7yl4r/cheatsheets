aed50937d6a2768c9e0ba836560b7281d236347c
Tylar <murray.tylar@gmail.com>
2019-01-20 13:11:57 -0500

    switch to read from req.txt

--- python/setup.py
+++ python/setup.py
@@ -4,9 +4,12 @@
 from setuptools import setup
 import io
 
-import projectname
+VERSION = '0.0.1' # should match __version__ in PROJECT_NAME.__init__.py
 
+
+# === long_description comes from README.md
 def read(*filenames, **kwargs):
+    """Helper fn to help get project info for setup()"""
     encoding = kwargs.get('encoding', 'utf-8')
     sep = kwargs.get('sep', '\n')
     buf = []
@@ -17,18 +20,28 @@ def read(*filenames, **kwargs):
 
 long_description = read('README.md') #, 'CHANGES.txt')
 
-setup(name='projectname',
-    version=projectname.__version__,
-    description='short desc of projectname',
-    long_description=long_description,
-    author='Tylar Murray',
-    author_email='code+projectname@tylar.info',
-    url='https://github.com/7yl4r/projectname',
+# === installation requirements read from requirements files
+# so these two commands are equivalent:
+# * `pip install .`
+# * `pip install -r requirements.txt`
+_tests_require = [
+    line.strip() for line in open('tests_requirements.txt')
+    if line.strip() and not line.strip().startswith('--')
+]
 
-    tests_require=['nose'],
-    install_requires=[
-        'networkx'  # or whatever
-    ],
+_install_requires = [
+    line.strip() for line in open('requirements.txt')
+    if line.strip() and not line.strip().startswith('--')
+]
+
+_extras_require = {  # allows install w/ `pip install .[test]`
+    'test': _tests_require
+}
+    
+setup(
+    name='projectname',
+    description='short desc of projectname',
+    url='https://github.com/7yl4r/projectname',
     entry_points={  # sets up CLI (eg bash) commands
         'console_scripts': [
             'projectname_cmd_name = projectname.import.path.to.my.cmd:method_name_in_that_module',
@@ -38,5 +51,14 @@ setup(name='projectname',
     packages=[  # modules that are added to python when this is installed
         'projectname', 
         'OtherProjectProvidedPackage2'
-    ]
+    ],
+    
+    # === no edits needed past here
+    author='Tylar Murray',
+    author_email='code+projectname@tylar.info',
+    version=VERSION,
+    long_description=long_description,
+    install_requires=_install_requires,
+    tests_require=_tests_require,
+    extras_require=_extras_require, 
 )
