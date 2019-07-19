3d548ae01975b891c3bbb83bd885d0f92836c097
Tylar <murray.tylar@gmail.com>
2019-07-19 16:44:53 +0000

    cleanup & expand examples

--- python/ExampleClass_test.py
+++ python/ExampleClass_test.py
@@ -1,54 +1,58 @@
 """
 example unit test for ExampleClass
 
-list of unittest assert methods: https://docs.python.org/3/library/unittest.html#assert-methods
+list of unittest assert methods:
+https://docs.python.org/3/library/unittest.html#assert-methods
 """
 
 # std modules:
 from unittest import TestCase
-try:
-    # py2
-    from mock import MagicMock
-except ImportError:
-    # py3
-    from unittest.mock import MagicMock
+from unittest.mock import patch
+from unittest.mock import MagicMock
+
+# tested module(s):
+from ExampleClass import ExampleClass
+
 
 class Test_ExampleClass_main(TestCase):
 
     # tests:
     #########################
-    @patch('ExampleClass.my_method')  # monkey patch to collect info on calls instead of actually calling
-    def test_calls_my_method(self, mock_my_method):
+    @patch('os.stat')  # patch to collect info instead of actually calling
+    def test_calls_my_method(self, mock_os_stat):
         """ ExampleClass module calls my_method twice """
-        with patch('os.walk') as mock_walk:  # another syntax for monkey patching
-         
-            # tested module(s):
-            from ExampleClass import ExampleClass
-            # NOTE: you must import AFTER monkey patching!!!
+        with patch('os.walk') as mock_walk:  # alt syntax for monkey patching
 
             mock_walk.return_value = [
-                '/home/username/my_folder', 
+                '/home/username/my_folder',
                 ('my_subfolder', 'my_other_subdir'),
-                ('file_in_my_folder.csv', 'files_in_subfolders_not_here_though.md')
+                ('file_in_my_folder.csv', 'files_in_subdirs_not_here_tho.md')
             ]
-        
-            # parse_args is argv[] parsing function (see `projectname_main.py`)
-            test_args = parse_args([  
-                '-vv',
-                'subcommand',
-                '--subcommand_arg',
-                '-k', 'my_key_val_arg_value',
-            ])
-            result = ExampleClass.stuff(test_args)
-        
-        self.assertEqual(mock_my_method.call_count, 2)
-        
+
+            # new return value for each call w/ side_effect:
+            mock_os_stat.side_effect = [
+                MagicMock(st_mode=11111, st_ino=11111111, st_dev=2049),
+                MagicMock(st_mode=22222, st_ino=22222222, st_dev=2049),
+            ]
+
+            eg_class_obj = ExampleClass()
+            # replace class method with MagicMock
+            eg_class_obj.class_method = MagicMock(
+                name='class_method',
+                side_effect=[1, 2, 3],
+                return_value=0
+            )
+            ExampleClass.other_class_method()
+
+            # expect class_method was called twice by other_class_method()
+            self.assertEqual(eg_class_obj.class_method.call_count, 2)
+
         # === other useful test patterns:
         # multiple values expected in result array:
         self.assertTrue(('yada', 'whatever') in result)
         self.assertDictContainsSubset(
             {
-                "my_key" : "my_value"
+                "my_key": "my_value"
             },
             my_dict_containing_at_least_key_vals_in_dict_above
         )
